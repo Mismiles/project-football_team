c5999599417ad2d582508d2940134c3da7a6e743
Msmiles <muhammadirfan123@hotmail.com>
2020-09-21 22:13:24 +0000

    attempted buidling product page and linking url for individual product items in product_detail.html

--- football/settings.py
+++ football/settings.py
@@ -163,6 +163,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'project-football'
